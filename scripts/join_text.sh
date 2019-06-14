cb2a45f686c5b82c4581a09f9fc220933b9e5f91
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-06-14 09:31:02 +0700

    join text script

--- scripts/join_text.sh
+++ scripts/join_text.sh
@@ -4,9 +4,11 @@ export DATASET=vibid
 export SPEAKER=mdpa
 
 export DATANAME=${DATASET}_${SPEAKER}
+rm $DATANAME.txt
 touch $DATANAME.txt
 
 cd $SPEAKER/
 for i in `ls *.txt`;do
-    cat $i >> ../$DATANAME.txt
+    export TEXT=$(cat $i)
+    echo $TEXT >> ../$DATANAME.txt
 done
