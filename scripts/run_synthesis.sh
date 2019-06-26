4ecaab656017e3fc1be94b4608631b75edeaa4d6
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-06-26 14:57:23 +0700

    update more details

--- scripts/run_synthesis.sh
+++ scripts/run_synthesis.sh
@@ -25,6 +25,8 @@ DATASET=vibid SPEAKER=fena
 
 export NMFILE="$(date +'%d%m%Y_%H%M')"
 
+sed -i "45s#all: data voice#all: data" Makefile
+
 make all 2>&1 | tee log_prepare_$NMFILE.txt
 
 scriptpath=$(pwd)
