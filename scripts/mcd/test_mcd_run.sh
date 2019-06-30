2eac5e407e1e9377a1dd6e9c009dce83d40489f5
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-06-30 13:24:04 +0700

    run mcd batch

---
+++ scripts/mcd/test_mcd_run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export DIRLIST=$(ls -1 data_test/)
+export RESFNAME=mcd_test_result.txt
+
+export PYTHONPATH=.
+chmod a+x bin/*
+
+echo "MCD Test Result" > $RESFNAME
+
+for i in `echo $DIRLIST`;do
+    echo "" >> $RESFNAME
+    echo "========================" >> $RESFNAME
+    echo "" >> $RESFNAME
+
+    echo "Testing ${i}" >> $RESFNAME
+    echo "Testing ${i}"
+
+    cat data_test/${i}/test_data/corpus.lst | \
+    xargs bin/get_mcd_plain_new \
+    data_test/${i}/test_data/ref-examples \
+    data_test/${i}/test_data/aligned-synth-examples | grep "overall MCD" >> $RESFNAME
+done
+
+
+
+
