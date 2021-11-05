7d16a0f396dc3a68f141ba7400d1e267a4d3fdb7
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-11-05 08:28:20 +0700

    update gcc -fcommon

--- scripts/install_archlinux.sh
+++ scripts/install_archlinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# sudo pacman -S tcsh gawk sox libx11 gcc-multilib 
+# sudo pacman -S tcsh gawk sox libx11 gcc-multilib
 # sudo pacman -U ncurses5-compat-libs*
 
 export SOURCE_DIR=~/HTS/install
@@ -40,42 +40,55 @@ tar zxvf $SOURCE_DIR/festvox-2.8.0-release.tar.gz -C $TARGET_DIR
 tar jxvf $SOURCE_DIR/flite-2.0.0-release.tar.bz2 -C $TARGET_DIR
 
 cd $TARGET_DIR/SPTK-3.10
-./configure --prefix=$TARGET_DIR
-make all
+./configure CC='gcc -fcommon' \
+            CXX='g++ -fcommon' \
+            CPP='gcc -E -fcommon' \
+            --prefix=$TARGET_DIR
+make -j$(nproc) all
 make install
 
 cd $TARGET_DIR/htk
 patch -p1 < ../HTS-2.3_for_HTK-3.4.1.patch
-./configure --prefix=$TARGET_DIR
-make all
+./configure CC='gcc -fcommon' --prefix=$TARGET_DIR
+make -j$(nproc) all
 make install
-make hdecode
+sed -i 's/        /\t/g' $TARGET_DIR/htk/HLMTools/Makefile
+make -j$(nproc) hlmtools
+make install-hlmtools
+make -j$(nproc) hdecode
+make -j$(nproc) hdecode
 make install-hdecode
 
 cd $TARGET_DIR/hts_engine_API-1.10
-./configure --prefix=$TARGET_DIR
-make all
+./configure CC='gcc -fcommon' \
+            CPP='gcc -E -fcommon' \
+            ac_ct_CC='gcc -fcommon' \
+            --prefix=$TARGET_DIR
+make -j$(nproc) all
 make install
 
 cd $TARGET_DIR/flite-2.0.0-release
-./configure --prefix=$TARGET_DIR
-make all
+./configure CC='gcc -fcommon' --prefix=$TARGET_DIR
+make -j$(nproc) all
 make install
 
 cd $TARGET_DIR/speech_tools
 ./configure --prefix=$TARGET_DIR
-make all
+sed -i 's#gcc#gcc -fcommon#g' $TARGET_DIR/speech_tools/config/compilers/gcc_defaults.mak
+sed -i 's#g++#g++ -fcommon#g' $TARGET_DIR/speech_tools/config/compilers/gcc_defaults.mak
+make -j$(nproc) all
 make install
 
 cd $TARGET_DIR/festival
 ./configure --prefix=$TARGET_DIR
-make all
+make -j$(nproc) all
+make -j$(nproc) all
 make install
 
 cd $TARGET_DIR/festvox
 ./configure --prefix=$TARGET_DIR
 sed -i "s#NUL)#NULL)#g" src/vc/src/sub/gmm_sub.cc
-make all
+make -j$(nproc) all
 
 export TOOLS_DIR=~/.hts_sptk
 export PATH=$TOOLS_DIR/bin:$PATH
